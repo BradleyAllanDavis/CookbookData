b60c3cc6a57320b8c913cf6f710425e5d4fc28c9
Hannah <ha.al.bu@gmail.com>
2016-11-17 19:01:50 -0600

    fix a couple typos, add file with sample data

---
+++ sql/addSampleData.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO Recipes values(1,"Bread and butter", "User added recipe", "Take bread, and put the butter on it", 0, NULL);
+INSERT INTO RecipeIngredients values(1,18060,1,"oz");
+INSERT INTO RecipeIngredients values(1,01145,12,"g");
+
+#
+INSERT INTO Recipes values(2,"Carla's Tomato Soup", "Make any modifications that suit you and the preferences of your eaters: Onions in place of shallots; carrots for fennel; add garlic; omit the cayenne; a splash of cream instead of butter, as you wish.", "Heat 2 Tbsp. oil in a Dutch oven or other large heavy pot over medium-low. Add shallots and sliced fennel and season with salt. Cook, stirring occasionally, until vegetables are very soft and just beginning to take on color, 20 minutes. Add fennel seeds, paprika, and cayenne, if using, and cook, stirring often, until incorporated and very fragrant, about 1 minute. Add tomatoes, crushing them with your hands as you go, season with salt and pepper, and bring to a simmer. Cook, stirring occasionally, until tomatoes are beginning to fall apart, 25–35 minutes.
+Transfer tomato mixture to a blender, add butter and 1 1/2 cups of water, and purée, adding more water if too thick, until smooth (or use an immersion blender directly in the pot). Taste and season with salt and pepper; return to pot. Keep warm, stirring occasionally, over low heat.
+Heat 2 Tbsp. oil in a medium skillet, preferably cast-iron, over medium-low. Fry 2 slices of bread until deep golden brown, about 2 minutes per side. Transfer to paper towels. Add 1 Tbsp. oil to skillet and repeat with another 2 slices of bread. Repeat with remaining 1 Tbsp. oil and remaining bread. Season with sea salt. Halve toast and serve with soup.", 0, NULL);
+INSERT INTO RecipeIngredients values(2,4053,6,"tablespoon");
+INSERT INTO RecipeIngredients values(2,11282,6,"small");
+INSERT INTO RecipeIngredients values(2,11957,1,"bulb");
+INSERT INTO RecipeIngredients values(2,11531,7,"cup");
+INSERT INTO RecipeIngredients values(2,2030,1,"tbsp, ground");
+INSERT INTO RecipeIngredients values(2,2028,2,"tsp");
+INSERT INTO RecipeIngredients values(2,2031,0.25,"tsp");
+INSERT INTO RecipeIngredients values(2,1145,3,"tbsp");
+INSERT INTO RecipeIngredients values(2,18967,6,"slice");
+INSERT INTO RecipeIngredients values(2,2047,2,"tbsp");
+
+

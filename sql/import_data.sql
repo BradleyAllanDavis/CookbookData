fde4a7dfb3742c3ce5ce2d37a5658aca6dbeea8f
Bradley <bad884@me.com>
2016-12-10 12:37:06 -0600

    Clean up fields for import

--- sql/import_data.sql
+++ sql/import_data.sql
@@ -1,9 +1,9 @@
 \copy cookbook_foodgroup (id, name) FROM '../usda/fd_group.tsv' DELIMITER E'\t';
-\copy cookbook_grammapping (ingredient, common_measure, amount_grams) FROM '../usda/weight_cleaned.tsv' DELIMITER E'\t';
-\copy cookbook_ingredientnutrient (ingredient, nutrient, amount) FROM '../usda/nut_data.tsv' DELIMITER E'\t';
-\copy cookbook_ingredient (id, food_group, name) FROM '../usda/ingredients.tsv' DELIMITER E'\t';
+\copy cookbook_grammapping (ingredient_id, common_measure, amount_grams) FROM '../usda/weight_cleaned.tsv' DELIMITER E'\t';
+\copy cookbook_ingredientnutrient (ingredient_id, nutrient_id, amount) FROM '../usda/nut_data.tsv' DELIMITER E'\t';
+\copy cookbook_ingredient (id, foodgroup_id, name) FROM '../usda/ingredients.tsv' DELIMITER E'\t';
 \copy cookbook_nutrient (id, unit, name) FROM '../usda/nutr_def.tsv' DELIMITER E'\t';
-\copy cookbook_recipeingredient (recipe, ingredient, unit, amount) FROM '../epicurious/recipe_ingredients.tsv' DELIMITER E'\t';
+\copy cookbook_recipeingredient (recipe_id, ingredient_id, unit, amount) FROM '../epicurious/recipe_ingredients.tsv' DELIMITER E'\t';
 \copy cookbook_recipe (id, title, description, instructions, serves) FROM '../epicurious/recipes_clean.tsv' DELIMITER E'\t';
 \copy cookbook_tag (tag_name) FROM '../epicurious/tags.tsv' DELIMITER E'\t';
-\copy cookbook_recipetag (recipe, tag) FROM '../epicurious/recipe_tags.tsv' DELIMITER E'\t';
+\copy cookbook_recipetag (recipe_id, tag_id) FROM '../epicurious/recipe_tags.tsv' DELIMITER E'\t';

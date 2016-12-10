6068b240387d4f70ca2966276a84a8b10ff3a356
Bradley <bad884@me.com>
2016-12-10 13:12:46 -0600

    Add missing commas

--- sql/import_data.sql
+++ sql/import_data.sql
@@ -3,7 +3,7 @@
 \copy cookbook_ingredientnutrient (id, ingredient_id, nutrient_id, amount) FROM '../usda/nut_data_w_id.tsv' DELIMITER E'\t';
 \copy cookbook_ingredient (id, foodgroup_id, name) FROM '../usda/ingredients.tsv' DELIMITER E'\t';
 \copy cookbook_nutrient (id, unit, name) FROM '../usda/nutr_def.tsv' DELIMITER E'\t';
-\copy cookbook_recipeingredient (id recipe_id, ingredient_id, unit, amount) FROM '../epicurious/recipe_ingredients_w_id.tsv' DELIMITER E'\t';
+\copy cookbook_recipeingredient (id, recipe_id, ingredient_id, unit, amount) FROM '../epicurious/recipe_ingredients_w_id.tsv' DELIMITER E'\t';
 \copy cookbook_recipe (id, title, description, instructions, serves) FROM '../epicurious/recipes_clean.tsv' DELIMITER E'\t';
 \copy cookbook_tag (tag_name) FROM '../epicurious/tags.tsv' DELIMITER E'\t';
-\copy cookbook_recipetag (id recipe_id, tag_id) FROM '../epicurious/recipe_tags_w_id.tsv' DELIMITER E'\t';
+\copy cookbook_recipetag (id, recipe_id, tag_id) FROM '../epicurious/recipe_tags_w_id.tsv' DELIMITER E'\t';

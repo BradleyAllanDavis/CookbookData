2381115b7dbcfb11db2e6106f3764f887e8f6902
Bradley <bad884@me.com>
2016-12-10 11:29:27 -0600

    Update for postgres

--- sql/import_data.sql
+++ sql/import_data.sql
@@ -1,9 +1,9 @@
-COPY cookbook_foodgroup (id, name) FROM '../usda/fd_group.tsv' DELIMITER E'\t';
-COPY cookbook_grammapping (ingredient, common_measure, amount_grams) FROM '../usda/weight_cleaned.tsv' DELIMITER E'\t';
-COPY cookbook_ingredientnutrient (ingredient, nutrient, amount) FROM '../usda/nut_data.tsv' DELIMITER E'\t';
-COPY cookbook_ingredient (id, food_group, name) FROM '../usda/ingredients.tsv' DELIMITER E'\t';
-COPY cookbook_nutrient (id, unit, name) FROM '../usda/nutr_def.tsv' DELIMITER E'\t';
-COPY cookbook_recipeingredient (recipe, ingredient, unit, amount) FROM '../epicurious/recipe_ingredients.tsv' DELIMITER E'\t';
-COPY cookbook_recipe (id, title, description, instructions, serves) FROM '../epicurious/recipes_clean.tsv' DELIMITER E'\t';
-COPY cookbook_tag (tag_name) FROM '../epicurious/tags.tsv' DELIMITER E'\t';
-COPY cookbook_recipetag (recipe, tag) FROM '../epicurious/recipe_tags.tsv' DELIMITER E'\t';
+\copy cookbook_foodgroup (id, name) FROM '../usda/fd_group.tsv' DELIMITER E'\t';
+\copy cookbook_grammapping (ingredient, common_measure, amount_grams) FROM '../usda/weight_cleaned.tsv' DELIMITER E'\t';
+\copy cookbook_ingredientnutrient (ingredient, nutrient, amount) FROM '../usda/nut_data.tsv' DELIMITER E'\t';
+\copy cookbook_ingredient (id, food_group, name) FROM '../usda/ingredients.tsv' DELIMITER E'\t';
+\copy cookbook_nutrient (id, unit, name) FROM '../usda/nutr_def.tsv' DELIMITER E'\t';
+\copy cookbook_recipeingredient (recipe, ingredient, unit, amount) FROM '../epicurious/recipe_ingredients.tsv' DELIMITER E'\t';
+\copy cookbook_recipe (id, title, description, instructions, serves) FROM '../epicurious/recipes_clean.tsv' DELIMITER E'\t';
+\copy cookbook_tag (tag_name) FROM '../epicurious/tags.tsv' DELIMITER E'\t';
+\copy cookbook_recipetag (recipe, tag) FROM '../epicurious/recipe_tags.tsv' DELIMITER E'\t';

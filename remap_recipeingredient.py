0c3ce5890351b8999f213b167cd4ab5371350a50
Questor96 <questor96@gmail.com>
2016-12-10 15:49:35 -0600

    swap ids for grammapping

---
+++ remap_recipeingredient.py
@@ -0,0 +1,20 @@
+recIngIn = open('./epicurious/recipe_ingredients_final.tsv', 'r')
+recIngOut = open('./epicurious/cookbook_recipeingredient.tsv', 'w')
+gramMapIn = open('./usda/weight_final.tsv', 'r')
+
+gramMapIdList = []
+
+for line in gramMapIn:
+    splitLine = line.split('\t')
+    ing_id = splitLine[1]
+    com_mes = splitLine[2]
+    line_id = splitLine[0]
+    gramMapIdList.append([(ing_id, com_mes),line_id])
+
+for line in recIngIn:
+    splitLine = line.split('\t')
+    ing_id = splitLine[2]
+    com_mes = splitLine[3]
+    for i in gramMapIdList:
+        if i[0] == (ing_id, com_mes):
+            recIngOut.write('\t'.join([splitLine[0], splitLine[1], i[1], splitLine[4]]))

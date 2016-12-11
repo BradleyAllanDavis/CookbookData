47eb3df62db1f0e6f02da404fec3261f97d60975
Bradley <bad884@me.com>
2016-12-10 19:23:48 -0500

    Clean ingredientnutrient

--- removebadingredients.py
+++ removebadingredients.py
@@ -4,6 +4,9 @@ recipIngredCleanFile = open('./epicurious/recipe_ingredients_final.tsv','w')
 gramMapFile = open('./usda/weight_cleaned_w_id.tsv', 'r')
 gramMapCleanFile = open('./usda/weight_final.tsv','w')
 
+ingredient_nutrient_file = open('./usda/nut_data_w_id.tsv','r')
+ingredient_nutrient_clean_file = open('./usda/nut_data_w_id_clean.tsv','w')
+
 ingList = {}
 
 for line in ingredFile:
@@ -20,8 +23,15 @@ for line in gramMapFile:
     if ing_id in ingList.keys():
         gramMapCleanFile.write(line)
 
+for line in ingredient_nutrient_file:
+    ing_id = line.split('\t')[1]
+    if ing_id in ingList.keys():
+        ingredient_nutrient_clean_file.write(line)
+
 ingredFile.close()
 recipIngredFile.close()
 recipIngredCleanFile.close()
 gramMapFile.close()
 gramMapCleanFile.close()
+ingredient_nutrient_file.close()
+ingredient_nutrient_clean_file.close()
